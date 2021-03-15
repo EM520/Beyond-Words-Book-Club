e2fd29cde308783102dffd6c5f6af18ba7e1d5de
punchcode <punchcode@punchcodes-MacBook-Pro.local>
2021-03-15 10:49:32 -0400

    update

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,12 +4,7 @@ import conn from '../db.js'
 const router = express.Router()
 router.get('/users', async (request, response) => {
   const users = await conn.raw(`SELECT * FROM users;`)
-<<<<<<< HEAD
-  respose.json(users.rows)
-  // response.json([{ id: 1, name: 'john' }])
-=======
   console.log(users.rows, 'users')
   response.json(users.rows)
->>>>>>> c2508b22c23947e495e35f40732cdd7109f978e0
 })
 export default router
