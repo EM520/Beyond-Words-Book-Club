31869fe04ff3883568288cc47d26738b23f8d13c
StefanieSeskin <stefanieaseskin@gmail.com>
2021-03-16 16:38:53 -0400

    styling and book carousel

--- client/src/features/genresbyuser/GenresByUser.js
+++ client/src/features/genresbyuser/GenresByUser.js
@@ -2,9 +2,69 @@ import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { selectGenreUser, getGenreUser } from "./genreuserSlice"
 import styles from "./GenreUser.module.css"
+import {Link} from "react-router-dom"
 
 export default function GenresByUser() {
-  const genreuser = useSelector(selectGenreUser)
+  // const genreuser = useSelector(selectGenreUser)
+  const genreuser = [
+    {
+      id: 1,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 2,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 3,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 4,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    },
+    {
+      id: 5,
+      cover_pic: "http://placehold.it/200x300",
+      name: 'fiction'
+    }
+
+    
+  ]
   console.log(genreuser, "gu")
   const dispatch = useDispatch()
   useEffect(() => {
@@ -12,14 +72,16 @@ export default function GenresByUser() {
   }, [])
   return (
     <>
+
       <div className = {styles.genreListMain}>
         {genreuser.map((genre) => (
           <div key={"guser-" + genre.id}  className = {styles.genreListsubMain}>
-            <div
+            {/* <div
               style={{ backgroundImage: `url(${genre.cover_pic})` }}
               className={styles.genreList}
-            ></div>
-            <p>{genre.name}</p>
+            ></div> */}
+            <p><img className={styles.genreList} src={genre.cover_pic} alt="Group Reading"  /></p>
+            <p><Link className={styles.genreUserLink} to="/genres">{genre.name}</Link></p>
           </div>
         ))}
       </div>
