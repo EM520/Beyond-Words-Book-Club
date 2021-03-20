40fd1cf297f20c82b1663a32d2b82aed7179e488
StefanieSeskin <stefanieaseskin@gmail.com>
2021-03-20 09:26:39 -0400

    photos on top 20

--- client/src/features/realtop20/RealTop20.js
+++ client/src/features/realtop20/RealTop20.js
@@ -3,11 +3,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import SearchBar from '../searchbar/SearchBar'
 import { selectTop20, getRealTop20 } from './realTop20Slice'
 import styles from './RealTop20.module.css'
-
+import RealTop20Footer from './RealTop20Footer'
 
 export default function RealTop20() {
   const top20 = useSelector(selectTop20)
   console.log(top20)
+  
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getRealTop20())
@@ -16,20 +17,23 @@ export default function RealTop20() {
     <>
    <SearchBar />
 
-      <div className={styles.top20main}>
-        {top20.map((top20) => (
-          <div key={'top20-' + top20.id}>
-            <img className={styles.bookList}
-            src={top20.cover_pic}
-            alt={top20.title}
-            className={styles.bookimage}/>
-          <div className={styles.description}>
-            {`${top20.first_name} ${top20.last_name} - "${top20.title}" (${top20.count} posts)`}              
-
-            </div>
+  <div className={styles.top20main}>
+    {top20.map((top20) => (
+      <div key={'top20-' + top20.id}>
+          <img className={styles.bookList}
+          src={top20.cover_pic}
+          alt={top20.title}
+          className={styles.bookimage}/>
+        <div className={styles.description}>
+            {`${top20.first_name} ${top20.last_name} - "${top20.title}" 
+            (${top20.count} posts)`}              
+        </div>
+          <div>
+            {/* <RealTop20Footer /> */}
           </div>
-        ))}
       </div>
-    </>
+    ))}
+  </div>
+  </>
   )
 }
