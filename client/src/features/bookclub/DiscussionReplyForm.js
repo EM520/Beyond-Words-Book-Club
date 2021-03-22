98bf03b50b64f067ec2ebac5e278c45579719815
mburg0s <burgos.marissa@gmail.com>
2021-03-22 16:58:10 -0400

    make some changes to discussion

--- client/src/features/bookclub/DiscussionReplyForm.js
+++ client/src/features/bookclub/DiscussionReplyForm.js
@@ -3,7 +3,7 @@ import { addDiscussion } from './bookclubSlice'
 import { useDispatch } from 'react-redux'
 import styles from './BookClub.module.css'
 
-export default function DiscussionReplyForm({ replies }) {
+export default function DiscussionReplyForm({ replies, id }) {
   const [input, setInput] = useState('')
   const dispatch = useDispatch()
 
@@ -15,6 +15,7 @@ export default function DiscussionReplyForm({ replies }) {
       parent_id: replies.id,
       discussion: input,
       group_id: replies.group_id,
+      id: id
     }
     dispatch(addDiscussion(discussion))
     setInput('')
