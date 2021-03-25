92e5e11c179ac4ac7b5cda46998cef6c4a58410b
mburg0s <burgos.marissa@gmail.com>
2021-03-25 16:50:02 -0400

    remove unnecessary files/folder

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
-import usersReducer from '../features/users/usersSlice'
+// import usersReducer from '../features/users/usersSlice'
 import authReducer from '../features/auth/auth'
-import genreuserReducer from '../features/genresbyuser/genreuserSlice'
+import genreuserReducer from '../features/genre/genreuserSlice'
 import bookclubReducer from '../features/bookclub/bookclubSlice'
 import profilesReducer from '../features/profile/profileSlice'
 import signupformReducer from '../features/auth/signupformSlice'
@@ -14,7 +14,7 @@ import searchReducer from '../features/search/searchSlice'
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    users: usersReducer,
+    // users: usersReducer,
     auth: authReducer,
     genreuser: genreuserReducer,
     profileState: profilesReducer,
