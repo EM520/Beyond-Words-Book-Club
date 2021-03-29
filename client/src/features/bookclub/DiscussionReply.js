b228a7d8a68321dc21221b4207ca1eb8788f2112
thomas1117 <thomasshannon117@yahoo.com>
2021-03-29 13:32:04 -0700

    ui tweaks

--- client/src/features/bookclub/DiscussionReply.js
+++ client/src/features/bookclub/DiscussionReply.js
@@ -6,10 +6,8 @@ export default function DiscussionReply(props) {
     <div>
       {props.replies.map((child) => (
         <li className={styles.replyView} key={'reply-' + child.id}>
-          <img src={child.photo} 
-               alt={child.username}
-          />
-          <div>
+          <img src={child.photo} alt={child.username} />
+          <div className={styles.commentUser}>
             <p className={styles.userdate}>
               {child.username} - {child.date}
             </p>
